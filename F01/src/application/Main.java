1263dc3205a0ad2a8a721907682f2326bba2d273
Eugen2000 <John.Hub.2016@gmail.com>
2016-07-22 16:02:06 +0300

    F001Scene

--- F01/src/application/Main.java
+++ F01/src/application/Main.java
@@ -2,6 +2,7 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
@@ -9,16 +10,20 @@ import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
+	public void start(Stage primaryStage) throws Exception {
+//		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			primaryStage.initStyle(StageStyle.TRANSPARENT);
+			primaryStage.setFullScreen(true);
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			
+			scene.setFill(null);
 			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 	
 	public static void main(String[] args) {
